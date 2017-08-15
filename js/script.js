6c04d0c2f1e75d4926322c239062a8d50c2bd4db
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-08-15 10:19:55 +0430

    fix : firebase added

--- js/script.js
+++ js/script.js
@@ -108,3 +108,16 @@ function end_game() {
 }
 
 
+function saveToFirebase(email) {
+    var emailObject = {
+        email: email
+    };
+
+    firebase.database().ref('subscription-entries').push().set(emailObject)
+        .then(function(snapshot) {
+            success(); // some success method
+        }, function(error) {
+            console.log('error' + error);
+            error(); // some error method
+        });
+}
